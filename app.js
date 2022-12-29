1e2a4d639a965a39d2f502a03fe4b15a8b05e432
telugujayaprakash <119862243+telugujayaprakash@users.noreply.github.com>
2022-12-29 22:44:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const toggleSwitch = document.querySelector(".toggle__input");
+
+// Check localStorage for saved theme
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  // Make sure toggle switch matches current theme
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplays) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+  let duration = Math.floor(interval/endValue);
+  let counter = setInterval(function(){
+    startValue += 1;
+    valueDisplays.textContent = startValue;
+    if (startValue == endValue){
+      clearInterval(counter);
+    } 
+  })
+})
+
+// login page
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
